bfb20d2659066467bde7cde1f5c56e5a21249f24
Mikhail751 <69088634+Mikhail751@users.noreply.github.com>
2021-02-26 20:10:28 +0300

    Добавьте файлы проекта.

---
+++ AutoThreadsCore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoMultithreading
+{
+    class AutoThreadsCore
+    {
+        public int Counter;
+        public object[] result;
+        public virtual object Work(object args) { return null; }
+        public object Run(object[] InputData)
+        {
+            Counter = InputData.Length;
+            result = new object[InputData.Length];
+            Thread[] streams = new Thread[result.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                streams[i] = new Thread(new ParameterizedThreadStart(LowWork));
+            }
+            for (int i = 0; i < result.Length; i++)
+            {
+                streams[i].Name = i.ToString();
+                streams[i].Start(InputData[i]);
+            }
+            while (Counter != 0) { continue; }
+            return MergeData(result);
+        }
+        private void LowWork(object data)
+        { 
+            this.result[Int32.Parse(Thread.CurrentThread.Name)] = Work(data);
+            this.Counter -= 1;
+        }
+        public virtual object MergeData(object[] data) { return null; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMultithreading
+{
+    class InData
+    {
+        public long s, e;
+        public InData(long start, long end)
+        {
+            this.s = start;
+            this.e = end;
+        }
+    }
+    class A : AutoThreadsCore
+    {
+        public override object Work(object args)
+        {
+            InData data = (InData)args;
+            long res = 0;
+            for (long i = data.s; i < data.e; i++)
+            {
+                res += i;
+            }
+            return res;
+        }
+        public override object MergeData(object[] data)
+        {
+            long sum = 0;
+            foreach (var item in this.result)
+            {
+                sum += (long)item;
+            }
+            return sum;
+        }
+    }
+    class Program
+    {
+        private static long x(long s, long e)
+        {
+            long res = 0;
+            for (long i = s; i < e; i++)
+            {
+                res += i;
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch s1 = new Stopwatch();
+            Stopwatch s2 = new Stopwatch();
+            long factor = 100000000;
+            A a = new A();
+            InData[] datas = new InData[4];
+            datas[0] = new InData(1, factor);
+            datas[1] = new InData(factor + 1, factor * 2);
+            datas[2] = new InData(factor * 2 + 1, factor * 3);
+            datas[3] = new InData(factor * 3 + 1, factor * 4);
+            s1.Start();
+            a.Run(datas);
+            s1.Stop();
+            s2.Start();
+            x(1, factor * 4);
+            s2.Stop();
+            double t1 = (double)s1.ElapsedMilliseconds / 1000;
+            double t2 = (double)s2.ElapsedMilliseconds / 1000;
+            Console.WriteLine($"MT time: {t1}, ST time: {t2} -> MT {t2 / t1} faster");
+        }
+    }
+}
